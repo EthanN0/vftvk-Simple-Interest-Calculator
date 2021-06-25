7999823c7c81d8f200e632dda381026b8de7f749
EthanN0 <85882629+EthanN0@users.noreply.github.com>
2021-06-25 14:48:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
-if (principal > 0) {
 document.getElementById("result").innerHTML = "If you deposit <span class='highlight'>" + principal + 
 "</span>,\<br\>at an interest rate of <span class='highlight'>" + rate + "</span>%\<br\>You will recieve an amount of <span class='highlight'>" + interest + "</span>,\<br\>in the year <span class='highlight'>" + year + "</span>";
-}else{
-document.getElementById("result").innerHTML = "Enter a positive number";
+if (principal <= 0) {
+alert("Enter a positive number");
+document.getElementById("principal").focus();
 }
 }
 
@@ -17,4 +17,4 @@ function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-}   
+} 
