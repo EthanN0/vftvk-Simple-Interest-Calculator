64396496bff90501f269bfc3a4e465ed6738981c
EthanN0 <85882629+EthanN0@users.noreply.github.com>
2021-06-25 15:26:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function compute()
 {
+//defining variables
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
+//Writing interest sentence based on variables
 document.getElementById("result").innerHTML = "If you deposit <span class='highlight'>" + principal + 
 "</span>,\<br\>at an interest rate of <span class='highlight'>" + rate + "</span>%\<br\>You will recieve an amount of <span class='highlight'>" + interest + "</span>,\<br\>in the year <span class='highlight'>" + year + "</span>";
+//if amount 0 or negative, dispay alert and take user back to text box
 if (principal <= 0) {
 alert("Enter a positive number");
 document.getElementById("principal").focus();
 }
 }
 
-function updateRate() 
+function updateRate() // Changes Rate Text when slider moved
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-} 
+}
